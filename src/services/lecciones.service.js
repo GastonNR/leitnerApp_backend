92f387e4382a65121fc85644d6ea6229e68b8247
GastonNR <nicoruiz987@gmail.com>
2026-05-15 12:11:00 -0300

    Incorporación de función de ordenar tarjetas en el el service

--- src/services/lecciones.service.js
+++ src/services/lecciones.service.js
@@ -36,9 +36,79 @@ function crearNuevaTarjeta(pregunta, respuesta) {
 
 }
 
-function ordenarTarjetasEnCajas(datos_tarjetas) {
-    console.log("Datos tarjetas en ordenar tarjetas: ", datos_tarjetas)
-    return datos_tarjetas
+function ordenarTarjetasEnCajas(datosTarjetas) {
+    //console.log("Datos tarjetas en ordenar tarjetas: ", datosTarjetas)
+    //console.log("tarjetas dentro de la lección: ", datosTarjetas.cajas[0].tarjetas)
+    const nuevasCajas = crearCajasVacias()
+    const fechaActual = new Date()
+
+    for (let i = 0; i < datosTarjetas.cajas.length; i++) {
+
+        for (let j = 0; j < datosTarjetas.cajas[i].tarjetas.length; j++) {
+
+            const tarjetaEvaluada = datosTarjetas.cajas[i].tarjetas[j]
+
+            if (tarjetaEvaluada.es_correcta === null){
+                nuevasCajas[i].tarjetas.push(tarjetaEvaluada)
+                continue
+            } 
+
+            if (tarjetaEvaluada.es_correcta === true) {
+
+                const proximaRevision = new Date(tarjetaEvaluada.proxima_revision)
+                tarjetaEvaluada.proxima_revision = proximaRevision
+                tarjetaEvaluada.ultima_revision = new Date()
+
+                switch (tarjetaEvaluada.nivel) {
+                    case 1:
+                        tarjetaEvaluada.nivel = 2
+                        tarjetaEvaluada.proxima_revision.setDate(tarjetaEvaluada.ultima_revision.getDate() + 3)
+                        nuevasCajas[1].tarjetas.push(tarjetaEvaluada)
+
+                        break;
+                    case 2:
+                        tarjetaEvaluada.nivel = 3
+                        tarjetaEvaluada.proxima_revision.setDate(tarjetaEvaluada.ultima_revision.getDate() + 7)
+                        nuevasCajas[2].tarjetas.push(tarjetaEvaluada)
+
+                        break;
+                    case 3:
+                        tarjetaEvaluada.nivel = 4
+                        tarjetaEvaluada.proxima_revision.setDate(tarjetaEvaluada.ultima_revision.getDate() + 14)
+                        nuevasCajas[3].tarjetas.push(tarjetaEvaluada)
+
+                        break;
+                    case 4:
+                        tarjetaEvaluada.nivel = 5
+                        tarjetaEvaluada.proxima_revision.setDate(tarjetaEvaluada.ultima_revision.getDate() + 30)
+                        nuevasCajas[4].tarjetas.push(tarjetaEvaluada)
+
+                        break;
+                    case 5:
+                        tarjetaEvaluada.nivel = 5
+                        tarjetaEvaluada.proxima_revision.setDate(tarjetaEvaluada.ultima_revision.getDate() + 30)
+                        nuevasCajas[4].tarjetas.push(tarjetaEvaluada)
+
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+
+                tarjetaEvaluada.ultima_revision = new Date()
+                const proximaRevision = new Date(tarjetaEvaluada.ultima_revision)
+
+                proximaRevision.setDate(proximaRevision.getDate() + 1)
+                tarjetaEvaluada.proxima_revision = proximaRevision
+            
+                tarjetaEvaluada.nivel = 1
+                nuevasCajas[0].tarjetas.push(tarjetaEvaluada)
+
+            }
+        }
+    }
+
+    return nuevasCajas
 
 }
 
@@ -151,8 +221,11 @@ export const updateBoxesService = async (usuarioId, leccionId, datosTarjetas) =>
     console.log("Datos tarjetas: ", datosTarjetas)
 
     try {
+
         const cajasNuevas = datosTarjetas.cajas
+
         if (!Array.isArray(cajasNuevas) || cajasNuevas.length !== 5) throw new Error("Se esperan exactamente 5 cajas")
+
         const usuario = await Usuario.findById(usuarioId)
         if (!usuario) throw new Error("Usuario no encontrado")
 
@@ -160,13 +233,21 @@ export const updateBoxesService = async (usuarioId, leccionId, datosTarjetas) =>
         if (!leccion) throw new Error("Lección no encontrada")
 
         const cajasOrdenadas = ordenarTarjetasEnCajas(datosTarjetas)
+
+        for (let i = 0; i < cajasOrdenadas.length; i++) console.log("Cajas ordenadas: ", cajasOrdenadas[i])
+
         leccion.cajas = cajasOrdenadas
+
+        //leccion = cajasOrdenadas
+
+        //console.log("Datos de las cajas de lección ya ordenadas: ", leccion)
         await usuario.save()
 
-        return leccion.cajas
+        return leccion
 
     } catch (error) {
-        throw new Error("Error interno al actualizar")
+        console.error(error)
+        throw new Error("Error interno al actualizar", error)
 
     }
 
